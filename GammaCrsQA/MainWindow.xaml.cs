c612a95379e7826cabb176c8ba1c34ef198a4f9c
JCat <jaksonlin@vip.qq.com>
2016-01-21 17:48:44 +0800

    EOD0121

--- GammaCrsQA/MainWindow.xaml.cs
+++ GammaCrsQA/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ namespace GammaCrsQA
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
 
         private NodeNetManager nodeNetManager;
