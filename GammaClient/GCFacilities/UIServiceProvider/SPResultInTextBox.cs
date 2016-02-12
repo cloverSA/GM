649475c2e46db4f51e3b6920a94c0e25100fe743
Bikachiu <jaksonlin@vip.qq.com>
2016-02-12 10:46:48 +0800

    POC 0212

--- GammaClient/GCFacilities/UIServiceProvider/SPResultInTextBox.cs
+++ GammaClient/GCFacilities/UIServiceProvider/SPResultInTextBox.cs
@@ -1,10 +1,12 @@
 ﻿using GammaClient.GCFacilities.TXManager;
+using GammaClient.GCHelpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace GammaClient.GCFacilities.UIServiceProvider
 {
@@ -52,7 +54,7 @@ namespace GammaClient.GCFacilities.UIServiceProvider
 
         #region UI Behavior
 
-        private void ScrollDownResult(object sender)
+        void ScrollDownResult(object sender)
         {
             var tb = sender as TextBox;
             if (tb != null)
@@ -62,6 +64,8 @@ namespace GammaClient.GCFacilities.UIServiceProvider
                 tb.ScrollToEnd();
             }
         }
+
+        public ICommand ScrollDownCommand { get { return new RelayCommand<object>(ScrollDownResult); } }
         #endregion
 
         #region tempalate Constructor
